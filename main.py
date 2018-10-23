91340ac22a23d23bbbde5ac24b45ffc7e6348281
anaypaul <anay.paul2@gmail.com>
2018-10-23 13:19:50 -0700

    Binary search on sorted array rotated by a pivot.

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ on the need.
 """
 from FindPeak import *
 def main():
-    arr = list(map(int, input().split()))
+    # arr = [5,6,7,1,2,3,4]
+    arr= []
     target = int(input())
     print("the key is present at index = ",search(arr,target))
+    # print("pivot element index = ",getPivot(arr))
 
 if __name__ == '__main__':
     main()
