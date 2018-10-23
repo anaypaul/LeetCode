accefecd6170f27aa886e26ff9e2f7fba9eb7568
anaypaul <anay.paul2@gmail.com>
2018-10-23 11:16:56 -0700

    Search in a sorted and rotated array of numbers using binary search.

--- FindPeak.py
+++ FindPeak.py
@@ -17,6 +17,7 @@ def getPivot(arr):
             low = mid + 1
     return mid
 def binarySearch(arr,low, high, key):
+    
     """
     Simple binary search implementation to search for a key in a sorted array.
     """
