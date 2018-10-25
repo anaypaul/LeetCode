b8f7a00f4a57bf8ca62fb8b41ad1bdc963bade33
anaypaul <anay.paul2@gmail.com>
2018-10-24 18:12:36 -0700

    main.py update

--- main.py
+++ main.py
@@ -3,13 +3,11 @@
 This is the main file to invoke the methods present in the solution files to each problem based
 on the need.
 """
-from FindPeak import *
+from SearchRotatedArray import *
 def main():
-    # arr = [5,6,7,1,2,3,4]
-    arr= []
+    arr= list(map(int, input().split()))
     target = int(input())
     print("the key is present at index = ",search(arr,target))
-    # print("pivot element index = ",getPivot(arr))
 
 if __name__ == '__main__':
     main()
