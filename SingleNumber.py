284f0511e1b3acd0a36256490baaaad45d5377b5
anaypaul <anay.paul2@gmail.com>
2018-10-31 20:31:03 -0700

    Find the unique element in a given array containing numbers in pairs.

--- SingleNumber.py
+++ SingleNumber.py
@@ -4,7 +4,7 @@ Note:
 Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
 """
 
-def SingleNumber(arr):
+def SingleNumber(nums):
     """
     :type nums: List[int]
     :rtype: int
