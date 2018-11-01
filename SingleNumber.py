e79f9d7172afd414d26057a81b9fd4f1e7a1b362
anaypaul <anay.paul2@gmail.com>
2018-10-31 20:29:36 -0700

    Find the unique number in a given array.

---
+++ SingleNumber.py
@@ -0,0 +1,15 @@
+"""
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+Note:
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+"""
+
+def SingleNumber(arr):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    ans = 0
+    for i in range(len(nums)):
+        ans = ans ^ nums[i]
+    return ans
