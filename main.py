1976fd516c277502252839a40e7ea603655f8232
anaypaul <anay.paul2@gmail.com>
2018-11-02 14:00:27 -0700

    main.py update for using mergeTwoLists method from MergeTwoSortedList.py

--- main.py
+++ main.py
@@ -3,12 +3,23 @@
 This is the main file to invoke the methods present in the solution files to each problem based
 on the need.
 """
-from SearchRotatedArray import *
-from RemoveDuplicatesFromSortedArray import removeDuplicates
-def main():
-    arr= list(map(int, input().split()))
-    print(removeDuplicates(arr))
-    print(arr)
+from MergeTwoSortedList import *
 
+
+
+def main():
+    l1 = ListNode(1)
+    l1.next = ListNode(2)
+    l1.next.next = ListNode(4)
+
+    l2 = ListNode(1)
+    l2.next = ListNode(5)
+    l2.next.next = ListNode(10)
+
+    ll = mergeTwoLists(l1, l2)
+    while(ll is not None):
+        print(ll.val,end="->")
+        ll = ll.next
+    print("Null")
 if __name__ == '__main__':
     main()
