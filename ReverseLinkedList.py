fd8b1a710f812fdfd9ad9e52940c1d0415dadb2d
anaypaul <anay.paul2@gmail.com>
2018-11-05 14:26:42 -0700

    Reverse Linked List.

---
+++ ReverseLinkedList.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+def reverseList(self, head):
+    """
+    :type head: ListNode
+    :rtype: ListNode
+    """
+    prev = None
+    curr = head
+    while(curr!= None):
+        # print(prev,curr.val,curr.next.val)
+        n = curr.next
+        curr.next = prev
+        prev = curr
+        curr = n
+    head = prev
+    return head
