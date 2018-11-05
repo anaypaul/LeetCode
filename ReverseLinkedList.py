546ee757922d680cad05cad76a39afe14a46adc5
anaypaul <anay.paul2@gmail.com>
2018-11-05 14:29:55 -0700

    Reverse a given Linked List.

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -3,7 +3,7 @@
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
-def reverseList(self, head):
+def reverseList(head):
     """
     :type head: ListNode
     :rtype: ListNode
