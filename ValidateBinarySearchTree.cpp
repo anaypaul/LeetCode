9b7ad2d4a15dde5676d199547f7f424be1ce4d50
anaypaul <anay.paul2@gmail.com>
2019-01-05 12:45:59 -0700

    Validate binary search tree

--- ValidateBinarySearchTree.cpp
+++ ValidateBinarySearchTree.cpp
@@ -9,18 +9,18 @@
  */
 class Solution {
 public:
-    bool validate(TreeNode *root, int low, int high){
+    bool validate(TreeNode *root,long long int min,long long int max){
         if(root==NULL){
             return true;
         }else{
-            if(low <= int(root->val) && int(root->val) <= right){
-                return validate(root->left, low , root->data - 1) && validate(root->right, root->data + 1 , right);
+            if(min < root->val && root->val < max){
+                return validate(root->left, min , root->val ) && validate(root->right, root->val, max);
             }else{
                 return false;
             }
         }
     }
     bool isValidBST(TreeNode* root) {
-        return validate(root,INT_MIN,INT_MAX);
+        return validate(root,INT64_MIN,INT64_MAX);
     }
 };
