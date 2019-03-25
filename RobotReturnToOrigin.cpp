954a3c50c28ed97ba59e96aef3660955df6a3ca1
anaypaul <anay.paul2@gmail.com>
2019-03-25 02:13:12 -0700

    Robot return to origin

---
+++ RobotReturnToOrigin.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int x = 0;
+        int y = 0;
+        for(int i = 0;i<moves.length();i++){
+            if(moves[i] == 'U'){
+                y++;
+            }else if(moves[i] == 'D'){
+                y--;
+            }else if(moves[i] == 'R'){
+                x++;
+            }else if(moves[i] == 'L'){
+                x--;
+            }
+        }
+        bool ans;
+        if(x==0 && y==0){
+            ans = true;
+        }else{
+            ans = false;
+        }
+        return ans;
+    }
+};
