6a908a9047e8fa75c7ac77f746c3196157de5f00
anaypaul <anay.paul2@gmail.com>
2019-03-26 02:34:44 -0700

    Increasing order search tree

---
+++ IncreasingOrderSearchTree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    TreeNode* increasingBST(TreeNode* root) {
+        vector<int> arr;
+        if(root == NULL){
+            return NULL;
+        }else{
+            TreeNode * curr = root;
+            while(curr!=NULL){
+                if(curr->left ==NULL){
+                    arr.push_back(curr->val);
+                    curr = curr->right;
+                }else{
+                    TreeNode * pre = curr->left;
+                    while(pre->right != curr && pre->right != NULL){
+                        pre = pre->right;
+                    }
+                    
+                    if(pre->right == NULL){
+                        pre->right = curr;
+                        curr = curr->left;
+                    }else{
+                        pre->right = NULL;
+                        arr.push_back(curr->val);
+                        curr = curr->right;
+                    }
+                }
+            }
+        }
+        TreeNode * r1 = new TreeNode(arr[0]);
+        TreeNode * curr = r1;
+        for(int i = 1;i<arr.size();i++){
+            curr->left = NULL;
+            curr->right = new TreeNode(arr[i]);
+            curr = curr->right;
+        }
+        return r1;
+    }
+};
