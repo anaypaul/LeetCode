ae2ec172a91f2092d36fed9b84e16da821221dcb
anaypaul <anay.paul2@gmail.com>
2019-04-03 10:08:55 -0700

    adding 2nd solution

--- FindFirstAndLastPositionOfElementInSortedArray.cpp
+++ FindFirstAndLastPositionOfElementInSortedArray.cpp
@@ -34,3 +34,28 @@ public:
         return res;
     }
 };
+//2nd solution using STL bsearch.
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res;
+        if(nums.empty()){
+            res.push_back(-1);
+            res.push_back(-1);
+            return res;
+        }
+        vector<int>::iterator lower,upper;
+        lower = lower_bound(nums.begin(), nums.end(), target );
+        upper = upper_bound(nums.begin(), nums.end(), target );
+        int start = lower - nums.begin();
+        int end = upper - nums.begin()-1;
+        if((start>=nums.size() || start<0)||nums[start] != target ){
+            res.push_back(-1);
+            res.push_back(-1);
+        }else{
+            res.push_back(start);
+            res.push_back(end);
+        }
+        return res;
+    }
+};
