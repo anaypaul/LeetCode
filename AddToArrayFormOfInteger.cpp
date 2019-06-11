359dc5c028991823de6dcd073f101f69deba5589
anaypaul <anay.paul2@gmail.com>
2019-06-10 20:49:50 -0700

    Add to array form of integer

--- AddToArrayFormOfInteger.cpp
+++ AddToArrayFormOfInteger.cpp
@@ -2,87 +2,31 @@ class Solution {
 public:
     vector<int> addToArrayForm(vector<int>& A, int K) {
         reverse(A.begin(), A.end());
-        for(int i = 0;i<A.size();i++){
-            cout<<A[i]<<" ";
-        }
-        cout<<endl;
-        
-        vector<int> num2;
-        while(K>0){
-            num2.push_back(K%10);
-            K = K/10;
-        }
-        for(int i = 0;i<num2.size();i++){
-            cout<<num2[i]<<" ";
-        }
-        cout<<endl;
-        
-        int i = 0;
         int carry = 0;
-        if(A.size() >= num2.size()){
-            cout<<"A is longer or equal"<<endl;
-            while(i<num2.size()){
-                int x = A[i] + num2[i] + carry;
-                if(x>9){
-                    carry = x/10;
-                    x = x%10;
-                }else{
-                    carry = 0;
-                }
-                A[i] = x;
-                i++;
-            }
-            while(i<A.size()){
-                int x = A[i] + carry;
-                if(x>9){
-                    carry = x/10;
-                    x = x%10;
-                }else{
-                    carry = 0;
-                }
-                A[i] = x;
-                i++;
-            }
-            if(carry != 0){
-                A.push_back(carry);
-            }
-            
-            cout<<"output"<<endl;
-            for(int i =0;i<A.size();i++){
-                cout<<A[i]<<" ";
-            }
+        int x = 0;
+        int i;
+        for(i = 0;i<A.size() && K>0;i++){
+            x = carry + A[i] + K%10;
+            K = K/10;
+            A[i] = x%10;
+            carry = x/10;
         }
-        else{
-            cout<<"num2 is longer"<<endl;
-            while(i<A.size()){
-                int x = A[i] + num2[i] + carry;
-                if(x>9){
-                    carry = x/10;
-                    x = x%10;
-                }
-                else{
-                    carry = 0;
-                }
-                A[i] = x;
-                i++;
-            }
-            while(i<num2.size()){
-                int x = num2[i] + carry;
-                if(x>9){
-                    carry = x/10;
-                    x = x%10;
-                }else{
-                    carry = 0;
-                }
-                A.push_back(x);
-                i++;
-            }
-            if(carry != 0){
-                A.push_back(carry);
-            }
+        while(i<A.size()){
+            x = carry + A[i] ;
+            A[i] = x%10;
+            carry = x/10;
+            i++;
+        }
+        while(K>0){
+            x = carry + K%10;
+            K = K/10;
+            A.push_back(x%10);
+            carry = x/10;
+        }
+        if(carry){
+            A.push_back(carry);
         }
         reverse(A.begin(), A.end());
-        
         return A;
     }
 };
