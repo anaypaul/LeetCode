72d3a8901210b91517bf43c346890acde22e5c80
anaypaul <anay.paul2@gmail.com>
2019-08-08 22:47:45 -0700

    New code sarching in binary tree

--- SearchInBinarySearchTree.cpp
+++ SearchInBinarySearchTree.cpp
@@ -9,6 +9,16 @@
  */
 class Solution {
 public:
+    TreeNode* searchBST2(TreeNode* root, int val) {
+        if(root == NULL || root->val == val){
+            return root;
+        }
+        if(root->val > val){
+            return searchBST(root->left, val);
+        }else{
+            return searchBST(root->right, val);
+        }
+    }
     TreeNode* searchBST(TreeNode* root, int val) {
         if(root == NULL){
             return root;
