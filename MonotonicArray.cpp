d7153d97a11e11da072eae99347e0bbd11c62f8d
anaypaul <anay.paul2@gmail.com>
2019-09-07 23:28:45 -0700

    Monotonic Array check with 1 pass

--- MonotonicArray.cpp
+++ MonotonicArray.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& A) {
-        bool res1 = true;
-        //check increasing
-        for(int i = 0;i < A.size() - 1; i++){
-            if(A[i]<= A[i+1]){
-                res1 = true;
-            }else{
-                res1 = false;
-                break;
-            }
-        }
-        //check decreasing
-        bool res2 = true;
+        bool increasing = true;
+        bool decreasing = true;
         for(int i = 0;i<A.size()-1;i++){
-            if(A[i] >= A[i+1]){
-                res2 = true;
-            }else{
-                res2 = false;
-                break;
+            if(A[i] < A[i+1]){
+                decreasing = false;
+            }
+            if(A[i] > A[i+1]){
+                increasing = false;
             }
         }
-        if(res2 || res1){
-            return true;
-        }
-        return false;
+        return decreasing || increasing;
     }
 };
