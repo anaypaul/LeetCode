4836b6533c452cb143c70227f811027877a8d0e3
anaypaul <anay.paul2@gmail.com>
2019-09-13 13:19:55 -0700

    Pascals Triangle

--- PascalsTriangle.cpp
+++ PascalsTriangle.cpp
@@ -7,7 +7,7 @@ public:
             return fun(i-1,j-1) + fun(i-1,j);
         }
     }
-    vector<vector<int>> generate(int numRows) {
+    vector<vector<int> > generate(int numRows) {
         vector<vector<int>> answer;
         for(int i = 1; i<=numRows; i++){
             vector<int> temp;
@@ -18,5 +18,32 @@ public:
             temp.clear();
         }
         return answer;
+    }
+};
+
+
+//Ierative solution 
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        if(numRows==0){
+            return res;
+        }
+        vector<int> curr;
+        curr.push_back(1);
+        res.push_back(curr);
+        for(int i = 2;i<=numRows;i++){
+            vector<int> temp;
+            temp.push_back(1);
+            for(int i = 0;i<curr.size()-1;i++){
+                temp.push_back(curr[i] + curr[i+1]);
+            }
+            temp.push_back(1);
+            res.push_back(temp);
+            curr = temp;
+            temp.clear();
+        }
+        return res;
     }
 };
