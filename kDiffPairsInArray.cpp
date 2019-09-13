6d86dae37620b27014a9df18840208317f4a4d12
anaypaul <anay.paul2@gmail.com>
2019-09-13 00:30:27 -0700

    K-Diff Pairs In Array updated using unordered map

--- kDiffPairsInArray.cpp
+++ kDiffPairsInArray.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
-        int res = 0;
-        set<pair<int,int>> s;
-        for(int i = 0;i<nums.size();i++){
-            for(int j = i+1;j<nums.size();j++){
-                if(abs(nums[i] - nums[j]) == k){
-                    int x = min(nums[i], nums[j]);
-                    int y = max(nums[i], nums[j]);
-                    pair<int,int> p (x,y);
-                    if(s.find(p) == s.end()){
-                        res++;
-                        s.insert(p);
-                    }
-                }
-            }
+        if(k<0) return 0;
+        unordered_map<int,int> m;
+        for(int n : nums){
+            m[n]++;
+        }
+        int res= 0;
+        
+        for(auto& p : m ){
+            res += k * m.count(p.first + k) || !k && p.second>1;
         }
         return res;
     }
