eacd412df40814c12bde757ee9fea20397cddbfc
anaypaul <anay.paul2@gmail.com>
2019-09-15 00:24:57 -0700

    Rotate Array updated

--- RotateArray.cpp
+++ RotateArray.cpp
@@ -12,3 +12,20 @@ public:
         }
     }
 };
+
+//Time Complexity : O(n*k)
+//Space Complexity : O(1)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int temp, prev;
+        for(int i = 0;i<k;i++){
+            prev = nums[nums.size()-1];
+            for(int j = 0;j<nums.size();j++){
+                temp = nums[j];
+                nums[j] = prev;
+                prev = temp;
+            }
+        }
+    }
+};
