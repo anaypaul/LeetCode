2d78bcfa4cfeb96ddf569751e3b1b25ef1d006d4
anaypaul <anay.paul2@gmail.com>
2019-09-16 15:51:33 -0700

    Reverse Linked List 2

--- ReverseLinkedList2.cpp
+++ ReverseLinkedList2.cpp
@@ -41,3 +41,103 @@ public:
         return head;
     }
 };
+
+//my version 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode * reverse(ListNode * head){
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode * rest = reverse(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        return rest;
+    }
+    int getLength(ListNode * head){
+        int len = 0;
+        while(head){
+            head = head->next;
+            len++;
+        }
+        return len;
+    }
+    ListNode* reverseBetween(ListNode* head, int m, int n) {
+        int len = getLength(head);
+        if(m==1 && n == len){
+            return reverse(head);
+        }else if(m==n){
+            return head;
+        }else{
+            if(m==1 && n<=len){
+                ListNode * curr = head;
+                ListNode * prev = NULL;
+                int i;
+                for(i = 1;i<n;i++){
+                    prev = curr;
+                    curr = curr->next;
+                }
+                ListNode * end = NULL;
+                end = curr->next;
+                curr->next = NULL;
+                head = reverse(head);
+                ListNode * temp = head;
+                while(temp->next){
+                    temp = temp->next;
+                }
+                temp->next = end;
+                return head;
+                
+            }
+        }
+        ListNode * prev1 = NULL;
+        ListNode * curr1 = head;
+        int i;
+        for(i = 1;i<m;i++){
+            prev1 = curr1;
+            curr1 = curr1->next;
+        }
+        ListNode * endOfFirstList = NULL;
+        if(prev1){
+            endOfFirstList = prev1; 
+        }else{
+            prev1 = NULL;
+        }
+        ListNode * temp_head = curr1;
+        
+        ListNode * prev2 = prev1;
+        ListNode * curr2 = curr1;
+        for(;i<n;i++){
+            prev2 = curr2;
+            curr2 = curr2->next;
+        }
+        
+        ListNode * startOfSecondList = curr2->next;
+        curr2->next = NULL;
+        if(endOfFirstList){
+            endOfFirstList->next = NULL;
+        }
+        temp_head = reverse(temp_head);
+        if(endOfFirstList){
+            endOfFirstList->next = temp_head;
+            ListNode * temp = endOfFirstList;
+            while(temp->next){
+                temp = temp->next;
+            }
+            temp->next = startOfSecondList;
+            ListNode * tt = startOfSecondList;
+            while(tt){
+                tt = tt->next;
+            }
+        }
+        return head;
+    }
+};
