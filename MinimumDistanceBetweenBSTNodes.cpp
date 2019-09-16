5a69da85254e742a6f97347bdd4234b5e4e2e9e3
anaypaul <anay.paul2@gmail.com>
2019-09-16 12:04:03 -0700

    Minimum Distance Between BST Nodes

--- MinimumDistanceBetweenBSTNodes.cpp
+++ MinimumDistanceBetweenBSTNodes.cpp
@@ -28,3 +28,33 @@ public:
         
     }
 };
+
+//Approach 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode * root, vector<int> & arr){
+        if(root){
+            dfs(root->left, arr);
+            arr.push_back(root->val);
+            dfs(root->right, arr);
+        }
+    }
+    int minDiffInBST(TreeNode* root) {
+        int ans = INT_MAX;
+        vector<int> arr;
+        dfs(root, arr);
+        for(int i = 0;i<arr.size()-1;i++){
+            ans = min(ans , arr[i+1] - arr[i]);
+        }
+        return ans;
+    }
+};
