7a2f2f96fe17cb2818218927772f513c2d40197d
anaypaul <anay.paul2@gmail.com>
2019-09-16 12:57:34 -0700

    Moving Average From Data Stream update

--- MovingAverageFromDataStream.cpp
+++ MovingAverageFromDataStream.cpp
@@ -1,33 +1,32 @@
 class MovingAverage {
 public:
     /** Initialize your data structure here. */
-    queue<double> q;
-    int size;
-    double sum;
+    queue<int> q;
+    int len = 0 ;
+    int x = 0;
+    int curr_sum = 0;
     MovingAverage(int size) {
-        this->size = size;
-        this->sum = 0;
+        x = size;
+        curr_sum = 0;
     }
     
     double next(int val) {
-        double avg;
-        if(q.size() < size){
-            q.push(val);
-            sum+= val;
-            avg = sum/q.size();
-        }else if(q.size() == size){
-            double t = q.front();
+        if(len + 1> x){
+            curr_sum -= q.front();
             q.pop();
             q.push(val);
-            sum = sum - t + val;
-            avg = sum/q.size();
+            curr_sum+=val;
+        }else{
+            q.push(val);
+            len++;
+            curr_sum += val;
         }
-        return avg;
+        return (double)curr_sum /(double) q.size();
     }
 };
 
 /**
  * Your MovingAverage object will be instantiated and called as such:
- * MovingAverage obj = new MovingAverage(size);
- * double param_1 = obj.next(val);
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
  */
