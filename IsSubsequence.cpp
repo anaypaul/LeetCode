0a66c8a2ea1d6cd7299071d0bd44e01856e19994
anaypaul <anay.paul2@gmail.com>
2019-09-18 00:55:01 -0700

    Is Subsequence updated with binary search for follow question

--- IsSubsequence.cpp
+++ IsSubsequence.cpp
@@ -20,3 +20,69 @@ public:
         return res;
     }
 };
+
+
+//Follow up - using binary search 
+class Solution {
+public:
+    int searchPos(vector<int> arr, int target){
+        int low = 0;
+        int high = arr.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid -1;
+            }
+        }
+        return low;
+    }
+    int bsearch(vector<int> arr, int target){
+        int low = 0;
+        int high = arr.size()-1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target){
+                return mid;
+            }else if( arr[mid] > target){
+                high = mid -1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        //not found so return the insert position.
+        int x = searchPos(arr, target);
+        
+        x = -1*(x) ;
+        x = x -1;
+        return x;
+    }
+    bool isSubsequence(string s, string t) {
+        vector<vector<int>> idx(256, vector<int>(0));
+        int n = idx.size();
+        int m = idx[0].size();
+        
+        for(int i = 0;i<t.length(); i++){
+            idx[t[i]-'a'].push_back(i);
+        }
+        
+        int last_index = 0;
+        for(int i = 0;i<s.length();i++){
+            
+            if(idx[s[i]-'a'].size() == 0){ //when the character is not present at all in the string t
+                return false;
+            }
+
+            int index = bsearch(idx[s[i]-'a'] , last_index);
+            if(index < 0 ){
+                index = -1*(index) -1;
+            }
+            if(index == idx[s[i]-'a'].size()){
+                return false;
+            }
+            last_index = idx[s[i]-'a'][index] + 1;
+        }
+        return true;
+    }
+};
