7cfb1fc7cfcd3c5d9e89ab9dd01195e40ab22e97
anaypaul <anay.paul2@gmail.com>
2019-09-19 11:47:30 -0700

    maximum array subarray1

--- MaximumArraySubarray1.cpp
+++ MaximumArraySubarray1.cpp
@@ -19,3 +19,24 @@ public:
         return (double)(res/(double)k);
     }
 };
+
+//Constant Space
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int total = 0;
+        int res = INT_MIN;
+        for(int i = 0;i<nums.size();i++){
+            if(i<k-1){
+                total += nums[i];
+            }else if(i== k-1){
+                total += nums[i];
+                res = max(res, total);
+            }else{
+                total += nums[i] - nums[i-k];
+                res = max(res, total);
+            }
+        }
+        return (double)(res)/k;
+    }
+};
