034000f6dcea2621969ee7e55383d549ba59bda5
anaypaul <anay.paul2@gmail.com>
2019-09-18 22:54:06 -0700

    Max distancde to closest person approach 2

--- MaximumDistanceToClosestPerson.cpp
+++ MaximumDistanceToClosestPerson.cpp
@@ -24,3 +24,34 @@ public:
         return getmax(dist/2, left, right) ;
     }
 };
+
+//Approach 2
+class Solution {
+public:
+    int maxDistToClosest(vector<int>& seats) {
+        int n = seats.size();
+        int K = 0;
+        int ans = 0;
+        for(int i = 0;i<n; i++){
+            if(seats[i] == 1){
+                K = 0;
+            }else{
+                K++;
+                ans = max(ans , (K+1)/2);
+            }
+        }
+        for(int i = 0 ;i<n ;i++){
+            if(seats[i] == 1){
+                ans = max(ans, i);
+                break;
+            }
+        }
+        for(int i = n-1;i>=0;--i){
+            if(seats[i] == 1){
+                ans = max(ans, n-i -1);
+                break;
+            }
+        }
+        return ans;
+    }
+};
