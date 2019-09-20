9fa096e22849fadd7770e1319c3029fbfba0fa6e
anaypaul <anay.paul2@gmail.com>
2019-09-20 11:23:37 -0700

    New Merge Intervals solution

--- MergeIntervals.cpp
+++ MergeIntervals.cpp
@@ -12,9 +12,6 @@ bool compareInterval(Interval i1, Interval i2) {
     } 
 class Solution {
 public:
-    // bool compareInterval(Interval i1, Interval i2) { 
-    //     return (i1.start < i2.start); 
-    // } 
     vector<Interval> merge(vector<Interval>& intervals) {
         vector<Interval> answer;
         sort(intervals.begin(), intervals.end() , compareInterval);
@@ -31,5 +28,34 @@ public:
             answer.push_back(Interval(start, end));
         }
         return answer;
+    }
+};
+
+//updated solution based on updated question and test cases. 
+class Solution {
+public:
+    static bool mycmp(vector<int>& a, vector<int>& b ){
+        if(a[0] < b[0]){
+            return true;
+        }else if(a[0] == b[0]){
+            if(a[1] < b[1]){
+                return true;
+            }
+        }
+        return false;
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> res;
+        sort(intervals.begin(), intervals.end(), mycmp);
+        for(int i = 0;i<intervals.size(); i++){
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            while(i+1 < intervals.size() && end >= intervals[i+1][0]){
+                end = max(intervals[i+1][1], end);
+                i = i + 1;
+            }
+            res.push_back({start, end});   
+        }
+        return res;
     }
 };
