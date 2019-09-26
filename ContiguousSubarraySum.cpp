7fd0ae717e2490733f374732a6215f97d806e6c6
anaypaul <anay.paul2@gmail.com>
2019-09-25 21:39:10 -0700

    update

--- ContiguousSubarraySum.cpp
+++ ContiguousSubarraySum.cpp
@@ -2,6 +2,27 @@ class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<int> sum_arr(0);
+        sum_arr.push_back(nums[0]);
+        for(int i = 1;i<n;i++){
+            sum_arr.push_back(nums[i] + sum_arr[i-1]);
+        }
+        for(int i = 0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                int sum = sum_arr[j] - sum_arr[i] + nums[i];
+                if(sum == k || (k != 0 && sum %k == 0 ) ){
+                    return true;
+                }
+            }
+        }
+        cout<<endl;
+        return false;
+    }
+};
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
         if(n<2){
             return false;
         }
