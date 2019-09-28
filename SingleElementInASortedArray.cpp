f9ad365d8844a156b039b8e927bfb5b494238634
anaypaul <anay.paul2@gmail.com>
2019-09-28 16:22:24 -0700

    Approach 2

--- SingleElementInASortedArray.cpp
+++ SingleElementInASortedArray.cpp
@@ -1,3 +1,6 @@
+//Approach 1
+//Time : O(n)
+//Space : O(1)
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
@@ -6,5 +9,25 @@ public:
             x = x^nums[i];
         }
         return x;
+    }
+};
+
+//Approach 2
+//Time : O(log n)
+//Space : O(1)
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int low = 0;
+        int high = nums.size()-1;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if((mid%2 == 1 && nums[mid] == nums[mid-1]) || ( mid%2== 0 && nums[mid] == nums[mid+1])){
+                low = mid + 1;
+            }else{
+                high = mid;
+            }
+        }
+        return nums[low];
     }
 };
