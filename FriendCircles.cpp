b065825b001e724541df9190c932be61f2a5f187
anaypaul <anay.paul2@gmail.com>
2019-10-05 18:35:41 -0700

    Friend Circles

---
+++ FriendCircles.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& M) {
+        int circles = 0;
+        vector<bool> visited(M.size(), false);
+        unordered_map<int, unordered_set<int>> graph;
+        for(int i = 0;i<M.size(); i++){
+            for(int j = 0; j<M[i].size(); j++){
+                if(M[i][j] == 1){
+                    graph[i].insert(j);
+                    graph[j].insert(i);
+                }
+            }
+        }
+        for(int i = 0; i<visited.size(); i++){
+            if(visited[i] == false){
+                circles++;
+                int start_node = i;
+                queue<int> q;
+                q.push(start_node);
+                visited[start_node] = true;
+                while(!q.empty()){
+                    int x= q.size();
+                    for(int j = 0;j<x; j++){
+                        int curr = q.front();
+                        for(int each : graph[curr]){
+                            if(visited[each] == false){
+                                q.push(each);
+                                visited[each] = true;
+                            }
+                        }
+                        q.pop();
+                    }
+                }
+            }
+        }
+        return circles;
+    }
+};
