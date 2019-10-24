ea00e09cf189391714549201d0af1702f0d23c05
anaypaul <anay.paul2@gmail.com>
2019-10-24 11:32:00 -0700

    Approach 2

--- MaximumWidthOfBinaryTree.cpp
+++ MaximumWidthOfBinaryTree.cpp
@@ -1,3 +1,4 @@
+//Approach 1 :[TLE] using BFS with TLE
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -55,6 +56,45 @@ public:
                 break;
             }
             vals.push_back(temp);
+        }
+        return res;
+    }
+};
+
+//Approach 2 :[ACCEPTED] BFS with TreeNode indexing stored as pair in a queue
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        queue<pair<TreeNode *, unsigned long long int >> q;
+        int res = 0;
+        if(root == NULL){
+            return res;
+        }
+        res = 1;
+        q.push(make_pair(root, 1));
+        while(!q.empty()){
+            int x = q.size();
+            int curr_width = q.back().second - q.front().second + 1;
+            res = max(res, curr_width);
+            for(int i = 0; i<x; i++){
+                auto t = q.front();
+                if(t.first->left){
+                    q.push(make_pair(t.first->left, t.second * 2));
+                }
+                if(t.first->right){
+                    q.push(make_pair(t.first->right, t.second * 2 + 1));
+                }
+                q.pop();
+            }
         }
         return res;
     }
