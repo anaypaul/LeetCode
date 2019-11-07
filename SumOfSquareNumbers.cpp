f6079e22b03e67375b6f56fa802549439c706cdf
anaypaul <anay.paul2@gmail.com>
2019-11-07 02:18:46 -0700

    approach 2

--- SumOfSquareNumbers.cpp
+++ SumOfSquareNumbers.cpp
@@ -12,3 +12,31 @@ public:
         return false;
     }
 };
+
+//Approach 2
+class Solution {
+public:
+    bool binarySearch(long long int low, long long int high, long long int key){
+        while(low<=high){
+            long long int mid = low + (high-low)/2;
+            if(mid * mid == key){
+                return true;
+            }else if(mid * mid < key){
+                low = mid +1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return false;
+    }
+    bool judgeSquareSum(int c) {
+        int x = (int)sqrt(c);
+        for(int i = 0; i<=x;i++){
+            int target = c - i*i;
+            if(binarySearch(0, target, target)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
