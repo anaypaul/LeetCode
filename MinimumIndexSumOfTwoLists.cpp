c08ef329aa15a6be33e736723d1c2d5e5eab566e
anaypaul <anay.paul2@gmail.com>
2019-11-16 23:35:23 -0700

    Minimum Index Sum of 2 lists

--- MinimumIndexSumOfTwoLists.cpp
+++ MinimumIndexSumOfTwoLists.cpp
@@ -26,3 +26,34 @@ public:
         return res;
     }
 };
+
+//approach 2
+class Solution {
+public:
+    static bool mycmp(pair<int,int> p1, pair<int,int> p2){
+        return p1.first + p1.second < p2.first+p2.second;
+    }
+    vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        vector<string> res;
+        unordered_map<string, int> m;
+        for(int i = 0; i<list1.size(); i++){
+            m[list1[i]] = i;
+        }
+        vector<pair<int, int>> v;
+        int sum = INT_MAX;
+        for(int j = 0; j<list2.size(); j++){
+            if(m.find(list2[j]) != m.end()){
+                v.push_back(make_pair(m[list2[j]],j));
+                sum = min(sum, m[list2[j]] +j);
+            }
+        }
+        for(int j = 0; j<list2.size(); j++){
+            if(m.find(list2[j]) != m.end() && sum == m[list2[j]] + j){
+                res.push_back(list2[j]);
+            }
+        }
+        return res;
+    }
+};
