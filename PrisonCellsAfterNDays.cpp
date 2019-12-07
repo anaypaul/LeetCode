df152602a0c1c9f4fe0be5f0e4c928bd74cf2f34
anaypaul <anay.paul2@gmail.com>
2019-12-06 20:34:15 -0700

    Prison cells after N days

--- PrisonCellsAfterNDays.cpp
+++ PrisonCellsAfterNDays.cpp
@@ -15,3 +15,41 @@ public:
         return mat[(N-1)%14];
     }
 };
+
+//Approach 2
+class Solution {
+public:
+    string encode(vector<int> v){
+        string res = "";
+        for(int i = 0; i<v.size(); i++){
+            res += to_string(v[i]) + "#";
+        }
+        return res;
+    }
+    vector<int> prisonAfterNDays(vector<int>& cells, int N) {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        vector<vector<int>> mat(0, vector<int>(8, 0));
+        unordered_set<string> myset;
+        while(true){
+            vector<int> next(8,0);
+            for(int i = 1; i<7; i++){
+                if(cells[i-1] == cells[i+1]){
+                    next[i] = 1;
+                }else{
+                    next[i] = 0;
+                }
+            }
+            string xx = encode(next);
+            if(myset.find(xx) == myset.end()){
+                myset.insert(xx);
+                mat.push_back(next);
+                cells = next;
+            }else{
+                break;
+            }
+        }
+        return mat[(N-1)%mat.size()];
+    }
+};
