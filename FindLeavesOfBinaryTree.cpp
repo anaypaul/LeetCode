7d7abbff2792e49ce13979a53a5f55f1114fa796
anaypaul <anay.paul2@gmail.com>
2019-12-10 16:51:08 -0700

    Find Leaves of binary tree

--- FindLeavesOfBinaryTree.cpp
+++ FindLeavesOfBinaryTree.cpp
@@ -54,3 +54,50 @@ public:
         return res;
     }
 };
+
+//Implementation 2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isLeaf(TreeNode * root){
+        return !root->left && !root->right;
+    }
+    void perform(TreeNode * root, vector<int>& v){
+        if(root!= nullptr){
+            if(root->left != nullptr && isLeaf(root->left)){
+                v.push_back(root->left->val);
+                root->left = nullptr;
+            }
+            if(root->right != nullptr && isLeaf(root->right)){
+                v.push_back(root->right->val);
+                root->right = nullptr;
+            }
+            perform(root->left, v);
+            perform(root->right, v);
+        }
+    }
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        vector<vector<int>> res;
+        if(root == nullptr){
+            return res;
+        }
+        while(root != nullptr && isLeaf(root) == false){
+            vector<int> v;
+            perform(root, v);
+            res.push_back(v);
+        }
+        res.push_back({root->val});
+        return res;
+    }
+};
