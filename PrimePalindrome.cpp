f7a32617338d9293dd276b1f9d7ddd79cde75077
anaypaul <anay.paul2@gmail.com>
2019-12-17 01:34:46 -0700

    Prime Palindromes

--- PrimePalindrome.cpp
+++ PrimePalindrome.cpp
@@ -35,3 +35,42 @@ public:
         return -1;
     }
 };
+// Approach 2
+class Solution {
+public:
+    bool isPrime(int n){
+        for(int i = 2; i*i<=n; i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    int genPalindrome(int num){
+        string s = to_string(num);
+        string temp = s;
+        reverse(temp.begin(), temp.end());
+        s.pop_back();
+        return stoi(s + temp);
+    }
+    int primePalindrome(int N) {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        int res = -1;
+        if(N<=2){
+            res = 2;
+        }else if(N>=8 && N<=11){
+            res = 11;
+        }else{
+            for(int i = 1; i<1000000; i++){
+                int temp = genPalindrome(i);
+                if(temp >= N && isPrime(temp)){
+                    res = temp;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
