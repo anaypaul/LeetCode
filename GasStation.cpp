6f48f5e83b8159b59dec39436443c44e5ed61c4c
anaypaul <anay.paul2@gmail.com>
2020-01-05 18:51:06 -0700

    Gas Station

--- GasStation.cpp
+++ GasStation.cpp
@@ -33,3 +33,29 @@ public:
         return -1;
     }
 };
+// Approach 2:
+// Time : O(n)
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        int tank = 0;
+        int curr_tank = 0;
+        int start = 0;
+        int n = gas.size();
+        for(int i = 0; i<n; i++){
+            tank += gas[i] - cost[i];
+            curr_tank += gas[i] - cost[i];
+            if(curr_tank < 0){
+                start = i + 1;
+                curr_tank = 0;
+            }
+        }
+        if(tank >=0){
+            return start;
+        }
+        return -1;
+    }
+};
