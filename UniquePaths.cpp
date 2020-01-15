7e05d11685a0255de3f9ea0369e1a69f5955e576
anaypaul <anay.paul2@gmail.com>
2020-01-14 21:40:26 -0700

    Unique Pathws

--- UniquePaths.cpp
+++ UniquePaths.cpp
@@ -1,3 +1,5 @@
+// TIme Complexity : O(n*m)
+//Space Complexity : O(n*m)
 class Solution {
 public:
     int uniquePaths(int m, int n) {
@@ -8,5 +10,37 @@ public:
             }
         }
         return dp[n-1][m-1];
+    }
+};
+
+//Space optimization
+// Space : O(2*n) ~ O(n) linear space
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> prev(m, 1);
+        vector<int> curr(m, 1);
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                curr[j] = prev[j] + curr[j-1];
+            }
+            swap(curr, prev);
+        }
+        return prev[m-1];
+    }
+};
+
+// Space Optimization 
+// Single linear array used to store the count
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<int> curr(m, 1);
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                curr[j] += curr[j-1];
+            }
+        }
+        return curr[m-1];
     }
 };
