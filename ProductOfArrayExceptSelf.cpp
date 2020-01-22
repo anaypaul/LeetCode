dcb66548bf442e814c326c6fc6a501ecc19a7d22
anaypaul <anay.paul2@gmail.com>
2020-01-22 13:03:54 -0700

    Product of array except self

--- ProductOfArrayExceptSelf.cpp
+++ ProductOfArrayExceptSelf.cpp
@@ -1,3 +1,5 @@
+//Time complexity : O(n)
+//Space complexity : O(n)
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& arr) {
@@ -13,6 +15,27 @@ public:
         for(int i = 0; i<arr.size(); i++){
             res.push_back(L[i] * R[i]);
         }
+        return res;
+    }
+};
+// Time Complexity : O(n)
+// Space Complexity : O(1)
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, 1 );
+        int p = 1;
+        for(int i = 1; i<n; i++){
+            res[i] = res[i-1] * nums[i-1];
+        }
+    
+        p = 1;
+        for(int i = n-2; i>=0; i--){
+            p *= nums[i+1];
+            res[i] *= p;
+        }
+        
         return res;
     }
 };
