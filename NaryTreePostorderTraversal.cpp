6d6b5b282f09103052e573a930e6e840b64e2009
anaypaul <anay.paul2@gmail.com>
2020-01-23 12:09:20 -0700

    N-ary Tree Postorder Traversal

--- NaryTreePostorderTraversal.cpp
+++ NaryTreePostorderTraversal.cpp
@@ -32,3 +32,65 @@ public:
         return res;
     }
 };
+
+//Iterative approach 1 using stack and reversing the array
+class Solution {
+public:
+    void dfs(Node * root, vector<int>& res){
+        if(root){
+            for(auto each : root->children){
+                dfs(each, res);
+            }
+            res.push_back(root->val);
+        }
+    }
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        stack<Node *> st;
+        if(root){
+            st.push(root);
+        }
+        while(!st.empty()){
+            auto curr = st.top();
+            st.pop();
+            res.push_back(curr->val);
+            for(int i = 0; i<curr->children.size(); i++){
+                st.push(curr->children[i]);
+            }
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
+
+//Iterative approach 2 only using stack
+class Solution {
+public:
+    void dfs(Node * root, vector<int>& res){
+        if(root){
+            for(auto each : root->children){
+                dfs(each, res);
+            }
+            res.push_back(root->val);
+        }
+    }
+    vector<int> postorder(Node* root) {
+        vector<int> res;
+        stack<pair<Node *, int>> st;
+        if(root){
+            st.push(make_pair(root, 0));
+        }
+        while(!st.empty()){
+            auto curr = st.top();
+            if(curr.second < curr.first->children.size()){
+                st.top().second = st.top().second + 1;
+                st.push(make_pair(curr.first->children[curr.second], 0));
+            }else{
+                res.push_back(curr.first->val);
+                st.pop();
+            }
+        }
+        return res;
+    }
+};
