2721fb335ff86131151e75170e4b5b020d39726d
anaypaul <anay.paul2@gmail.com>
2020-01-24 22:35:00 -0700

    Implementation using semaphore

--- PrintInOrder.cpp
+++ PrintInOrder.cpp
@@ -26,3 +26,39 @@ public:
         printThird();
     }
 };
+
+
+//Implementation using semaphore
+#include<semaphore.h>
+class Foo {
+public:
+    sem_t firstJobDone;
+    sem_t secondJobDone;
+    Foo() {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        sem_init(&firstJobDone, 0, 0);
+        sem_init(&secondJobDone, 0, 0);
+    }
+
+    void first(function<void()> printFirst) {
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        sem_post(&firstJobDone);
+    }
+
+    void second(function<void()> printSecond) {
+        sem_wait(&firstJobDone);
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        sem_post(&secondJobDone);
+    }
+
+    void third(function<void()> printThird) {
+        sem_wait(&secondJobDone);
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    
+    }
+};
