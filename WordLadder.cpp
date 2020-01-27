501ac45992e885e1307c9744b85386ef56ee769f
anaypaul <anay.paul2@gmail.com>
2020-01-26 20:35:14 -0700

    Word ladder bi-directional bfs

--- WordLadder.cpp
+++ WordLadder.cpp
@@ -79,3 +79,76 @@ public:
         return diff == 1;
     }
 };
+
+//Implementation 2 :
+//Bi-directional BFS 
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        //search if the endWord is present in the word list 
+        //if not, no point in searching, just return 0
+        if(find(wordList.begin(), wordList.end(), endWord) == wordList.end()) return 0;
+        
+        //now we would need 2 queue for maintaining the current level and the next level
+        //each for 1 direction. 
+        
+        // curr0 = current level at beginning of the graph.(BFS originating at beginWord)
+        // next0 = next level of curr0
+
+        //curr1 = current level at end of the graph.(BFS originating at endWord)
+        //next1 = next level of curr1
+
+        //we will also maintain pointers for executing bfs alternating between top 
+        //bottom ie the two directions
+        // *curr will hold the address of the queue currently being used for bfs
+        // *next will hold the address of the next queue currently being used for bfs
+        // and this will keep alternating at each level wise iteration of the bfs
+        unordered_set<string> visited0, visited1, *v0(&visited0), *v1(&visited1);
+        queue<string> curr0, next0, curr1, next1, *curr, *next;
+        
+        curr0.push(beginWord);
+        curr1.push(endWord);
+        
+        visited0.insert(beginWord);
+        visited1.insert(endWord);
+        
+        int len = 2;
+        bool flag = true;
+        
+        while(!curr0.empty() && !curr1.empty()){
+            curr = flag ? &curr0 : &curr1;
+            next = flag ? &next0 : &next1;
+            string node = curr->front();
+            curr->pop();
+            for(auto& x : wordList){
+                if(v0->count(x) || !isNeighbor(x, node)){
+                    continue;
+                }
+                if(v1->count(x)){
+                    return len;
+                }
+                v0->insert(x);
+                next->push(x);
+            }
+            if(curr->empty()){
+                //increment lenth of the path and swtich the direction of BFS
+                // by swithing between the queue for curr level , next level and 
+                // visited set by changing the flag value 
+                len++;
+                swap(*curr, *next);
+                swap(v0, v1);
+                flag = !flag;
+            }
+        }
+        return 0;
+    }
+    
+    bool isNeighbor(string& a, string& b){
+        int diff = 0;
+        for(int i = 0; i < a.size(); i++) if(a[i] != b[i] && ++diff > 1) return false;
+        return diff == 1;
+    }
+};
