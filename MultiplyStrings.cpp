c465568dd8b0caba95f93bf0b3923f9d930e871d
anaypaul <anay.paul2@gmail.com>
2020-01-30 12:29:02 -0700

    Multiply Strings

--- MultiplyStrings.cpp
+++ MultiplyStrings.cpp
@@ -73,3 +73,95 @@ public:
         return ans;
     }
 };
+
+//2nd Implementation
+class Solution {
+public:
+    string add(string s1, string s2){
+        string res = "";
+        if(s1.size() == 0){
+            return s2;
+        }
+        if(s2.size() == 0){
+            return s1;
+        }
+        if(s1.size() == 0 && s2.size() == 0){
+            return "";
+        }
+        int carry = 0;
+        int i = s1.size()-1;
+        int j = s2.size()-1;
+        while(i>=0 && j>=0){
+            int sum = s1[i] - '0' + s2[j] - '0' + carry;
+            res += to_string(sum %10);
+            carry = sum / 10;
+            i--;
+            j--;
+        }
+        while(i>=0){
+            int sum = s1[i] - '0' + carry;
+            res += to_string(sum % 10);
+            carry = sum / 10;
+            i--;
+        }
+        while(j>=0){
+            int sum = s2[j] - '0' + carry;
+            res += to_string(sum % 10);
+            carry = sum / 10;
+            j--;
+        }
+        if(carry){
+            res += to_string(carry);
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+    string multiply(string num1, string num2) {
+        std::ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+        int allZero = 0;
+        int n = num1.size();
+        int m = num2.size();
+        int i = n-1;
+        int j = m-1;
+        int c = 0;
+        bool flag = false;
+        for(int i = 0; i<num1.size(); i++){
+            allZero += num1[i] -'0';
+        }
+        if(allZero == 0){
+            flag = true;
+        }
+        allZero = 0;
+        for(int j = 0; j<num2.size(); j++){
+            allZero += num2[j] - '0';
+        }
+        if(allZero == 0){
+            flag = true;
+        }
+        string res = "";
+        if(flag){
+            return "0";
+        }
+        for(i = n-1; i>=0; i--){
+            string t = "";
+            int carry = 0;
+            for(j = m-1; j>=0; j--){
+                int sum = (num1[i] - '0') * (num2[j] - '0') + carry;
+                t += to_string(sum %10);
+                carry = sum / 10;
+            }
+            if(carry){
+                t += to_string(carry);
+            }
+            reverse(t.begin(), t.end());
+            for(int k = 0; k<c; k++){
+                t += to_string(0);
+            }
+            c++;
+            res = add(res, t);
+        }
+        return res;
+    }
+};
