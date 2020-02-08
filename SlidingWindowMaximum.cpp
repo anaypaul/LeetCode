5c92dc2f0ab8d601cfff276769d47aa9ab623d3e
anaypaul <anay.paul2@gmail.com>
2020-02-07 21:04:26 -0700

    Sliding window maximum O(n)

--- SlidingWindowMaximum.cpp
+++ SlidingWindowMaximum.cpp
@@ -1,3 +1,21 @@
+//Approach 1 : using stl 
+// Time : O(n^2)
+//Space : O(1)
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        int n = nums.size();
+        if(n == 0){
+            return res;
+        }
+        for(int i = 0; i+k<=n; i++){
+            res.push_back(*max_element(nums.begin()+i, nums.begin()+i+k));
+        }
+        return res;
+    }
+};
+//Implementation 2
 class Solution {
 public:
     int query(int start, int end, vector<int>& nums){
@@ -20,5 +38,45 @@ public:
             res.push_back(query(i, i+k -1, nums));
         }
         return res;
+    }
+};
+
+//Approach 2 : Linear time solution
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(k == 1 || n == 0){
+            return nums;
+        }
+        
+        vector<int> left(n);
+        left[0] = nums[0];
+        vector<int> right(n);
+        right[n-1] = nums[n-1];
+        
+        for(int i = 1; i<n; i++){
+            //left part
+            if(i%k == 0){
+                left[i] = nums[i];
+            }else{
+                left[i] = max(left[i-1], nums[i]);
+            }
+            
+            //right part
+            int j = n-1-i;
+            if((j+1) % k == 0){
+                right[j] = nums[j];
+            }else{
+                right[j] = max(right[j+1], nums[j]);
+            }
+        }
+        
+        vector<int> res;
+        for(int i = 0; i<n-k + 1; i++){
+            res.push_back(max(left[i+k-1], right[i]));
+        }
+        return res;
+
     }
 };
