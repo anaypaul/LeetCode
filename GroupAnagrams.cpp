cf8e49e0fe0bdd7c9532185b748dd38aa11d3f18
anaypaul <anay.paul2@gmail.com>
2020-02-09 09:06:07 -0700

    Group Anagrams

--- GroupAnagrams.cpp
+++ GroupAnagrams.cpp
@@ -1,62 +1,56 @@
+//Approach 1:
+// Time Complexity : O(N * KlogK)
+// Space Complexity : O(N*K)
 class Solution {
 public:
-    char getInt(int n){
-        if(n==0){
-            return '0';
-        }else if(n==1){
-            return '1';
-        }else if(n==2){
-            return '2';
-        }else if(n==3){
-            return '3';
-        }else if(n==4){
-            return '4';
-        }else if(n==5){
-            return '5';
-        }else if(n==6){
-            return '6';
-        }else if(n==7){
-            return '7';
-        }else if(n==8){
-            return '8';
-        }else if(n==9){
-            return '9';
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> m;
+        for(auto x : strs){
+            string t = x;
+            sort(t.begin(), t.end());
+            m[t].push_back(x);
         }
-        return -1;
+        for(auto x : m){
+            vector<string> t;
+            for(auto y : x.second){
+                t.push_back(y);
+            }
+            res.push_back(t);
+        }
+        return res;
     }
-    string getString(int n){
+};
+
+//Approach 2: 
+//Time Complexity : O(N * K), N = numbet of strings , K = length of the longest string 
+//Space Complexity : O(N * K)
+class Solution {
+public:
+    string vec2string(vector<int> hash){
         string res = "";
-        while(n > 0){
-            res = getInt(n%10) + res;
-            n = n/10;
+        for(auto t : hash){
+            res += to_string(t) + "#";
         }
         return res;
     }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> res;
-        unordered_map<string, vector<int>> um;
-        vector<int> hash(26, 0);
-        for(int i = 0; i<strs.size(); i++){
-            for(auto y : strs[i]){
-                hash[y-'a']++;
+        unordered_map<string, vector<string>> m;
+        for(auto x : strs){
+            vector<int> hash(26, 0);
+            for(auto e : x){
+                hash[e-'a']++;
             }
-            string xx = "";
-            for(auto val : hash){
-                xx = xx + getString(val) + ":";
-            }
-            for(int i = 0; i<hash.size(); i++){
-                xx = xx + getString(hash[i]) + ":";
-                hash[i] = 0;
-            }
-            um[xx].push_back(i);
+            string t = vec2string(hash);
+            m[t].push_back(x);
         }
-        
-        for(auto x : um){
-            vector<string> temp;
+        for(auto x : m){
+            vector<string> t;
             for(auto y : x.second){
-                temp.push_back(strs[y]);
+                t.push_back(y);
             }
-            res.push_back(temp);
+            res.push_back(t);
         }
         return res;
     }
