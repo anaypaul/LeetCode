f217727af1048dafaedee948c66df1d790bb91d7
anaypaul <anay.paul2@gmail.com>
2020-02-14 16:47:37 -0800

    Count aquare submatrices with all ones

--- CountSquareSubmatricesWithAllOnes.cpp
+++ CountSquareSubmatricesWithAllOnes.cpp
@@ -22,3 +22,40 @@ public:
         return res;      
     }
 };
+
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int res = 0;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        
+        for(int i = 0; i<matrix.size(); i++){
+            for(int j = 0; j<matrix[0].size(); j++){
+                if(matrix[i][j] == 1){
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        
+        for(int i = 1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(dp[i][j] == 1){
+                    dp[i][j] = min(dp[i][j-1], min(dp[i-1][j-1], dp[i-1][j])) + 1;
+                }else{
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                cout<<dp[i][j]<<" ";
+                res += dp[i][j];
+            }
+            cout<<endl;
+        }
+        return res;
+    }
+};
