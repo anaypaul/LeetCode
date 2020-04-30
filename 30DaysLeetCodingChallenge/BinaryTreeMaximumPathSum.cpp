adb50a8a1ab030df53391fc751c4557dbfc13c9a
anaypaul <anay.paul2@gmail.com>
2020-04-30 10:50:16 -0700

    BInary Tree Maximum Path Sum

---
+++ 30DaysLeetCodingChallenge/BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode * root, int & res){
+        if(!root){
+            return 0;
+        }
+        int leftSubtree = max(dfs(root->left, res), 0);
+        int rightSubtree = max(dfs(root->right, res), 0);
+        int val = root->val + leftSubtree + rightSubtree;
+        res = max(res, val);
+        return root->val + max(leftSubtree , rightSubtree);
+    }
+    int maxPathSum(TreeNode* root) {
+        int res = INT_MIN;
+        dfs(root, res);
+        return res;
+    }
+};
